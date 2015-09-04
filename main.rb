1267a4ef7eefba7a1e3c2690ff916b0c08ad9224
Barbara Monzo <barb@monzodesign.com>
2015-09-04 13:20:48 -0400

    added files to the project folder

--- main.rb
+++ main.rb
@@ -1,13 +1,50 @@
+require 'sinatra'
+
+
+get '/index' do 
+	erb :index
+end
+
+get '/gallery' do 
+	erb :gallery
+end
+
+get '/registration' do 
+	erb :registration
+end
+
+get '/contact' do 
+	erb :calendar
+end
+
+get '/important' do 
+	erb :calendar
+end
+
+get '/volunteer' do 
+	erb :calendar
+end
+
+get '/calendar' do 
+	erb :calendar
+end
+
+get '/sponsors' do 
+	erb :calendar
+end
+
+def send_mail
 
 require 'mandrill'
 m = Mandrill::API.new
 message = {
 	:subject=> "Hello from the Mandrill API",
-	:from_name=> "Barbara Monzo",
-	:text=>"Hi message, how are you?",
+	:from_name=> "name",
+	:text=>"message",
 	:to=>[{:email=> "barb@monzodesign.com", :name=> "Barbara"}],
 	:html=>"<html><h1>Hello<strong>Barbara,</strong>,<br>If you survive this WDI course, it will be a <strong>miracle</strong>!!!!!!</h1></html>",
 	:from_email=>"barb@monzodesign.com"
 	}
 	sending = m.messages.send message
 	puts sending
+end
