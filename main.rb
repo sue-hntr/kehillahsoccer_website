497b33c8afad8e8822fc6467a42afeb9fbaf99ff
Barb Monzo <barb@monzodesign.com>
2015-09-08 00:51:51 -0400

    added all of the content to the erb pages worked on volunteer registration form, added some CSS bits to style.css for the volunteer form and for the sponsors page

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 		erb :index
 	end
 
-	get '/volunteer.erb' do 
+	get '/volunteer' do 
 		erb :volunteer
 	end
 
@@ -20,7 +20,7 @@ require 'sinatra'
 		erb :gallery
 	end
 
-	get '/important.erb' do 
+	get '/important' do 
 		erb :important
 	end
 
@@ -30,10 +30,44 @@ require 'sinatra'
 
 	get '/register' do 
 		erb :register
+	end
 
 	get '/sponsors' do 
 		erb :sponsors
 	end
 
 	
+# This code is saying "GET to this location on my server and DO the following... load the form from the form.erb file."
+
+get '/' do 
+	erb :volunteer
+end
+
+
+# This code is saying "the content that gets POSTed to this form should DO the following... capture info in the forms indicated in the fields below... send email This will inspect the parameters from my form. 
+
+post "/volunteer" do
+	puts params.inspect
+	name = params["name"]
+	email = params["email"]
+	message = params["text"]
+	# coach = params["coach"]
+	send_email(name, email, message)
+end
+
+# the actual email content details here. We're using Mandrill required code.
+
+def send_email(name, email, message)
+
+	require 'mandrill'
+	m = Mandrill::API.new
+	message = {
+		:subject=> "Kehillah Soccer Inquiry...",
+		:from_name=> name,
+		:text=> message,
+		:to=>[{:email=> "bmonzo@gmail.com", :name=> "Barbara"}],
+		:from_email=> email
+	}
+	sending = m.messages.send message
+	puts sending
 end
